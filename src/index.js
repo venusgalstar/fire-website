6ee7f72f9ede1a57b7564d1d77bdda9f3eb410b2
kmd-code <duvall.kaydon@gmail.com>
2022-05-18 15:52:43 -0600

    v3-dynamic | setting up dynamic site values

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from "./App"
 import store from "./store/index"
+import { ContractContext } from './context/ContractContext';
 
 import { Provider } from 'react-redux';
 import { ParallaxProvider } from "react-scroll-parallax";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 window.store = store;
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <ParallaxProvider>
-            <ToastContainer />
-            <App />
-        </ParallaxProvider>
+        <ContractContext>
+            <ParallaxProvider>
+                <ToastContainer />
+                <App />
+            </ParallaxProvider>
+        </ContractContext>
     </Provider>,
     document.getElementById("root")
 );
--- src/pages/Main/MainInfo/index.jsx
+++ src/pages/Main/MainInfo/index.jsx
@@ -1,10 +1,12 @@
-import React, {useCallback} from "react";
-
+import React, { useContext } from "react";
 import './styles.scss'
 import { connect } from "react-redux";
 import RtChart from "../../../components/common/rt_chart";
+import { ContractNumbers } from "../../../context/ContractContext";
 
 const MainInfo = (props) => {
+    const contractValues = useContext(ContractNumbers)
+
     return <section id="main-info" className="main-info">
         <div className="main-info__inner">
             <div className="card-counters-wrapper">
@@ -18,7 +20,7 @@ const MainInfo = (props) => {
                 <div className="card-counter">
                     <div className="card-counter__title">Daily REWARDS</div>
                     <div className="card-counter__content">
-                        <div><span className="redHigh">~</span> $FIRE</div>
+                        <div><span className="redHigh">{contractValues.nestRewardRate}</span> $FIRE</div>
                         <div>Per nest</div>
                     </div>
                 </div>
@@ -47,12 +49,12 @@ const MainInfo = (props) => {
                         </a>
                         </div> */}
                         <div>
-                        <a className="traderLink"
-                            href="https://traderjoexyz.com/trade?inputCurrency=AVAX&outputCurrency=0xfcc6ce74f4cd7edef0c5429bb99d38a3608043a5#/"
-                            target="_blank"
-                        >
-                            TraderJoe
-                        </a>
+                            <a className="traderLink"
+                                href="https://traderjoexyz.com/trade?inputCurrency=AVAX&outputCurrency=0xfcc6ce74f4cd7edef0c5429bb99d38a3608043a5#/"
+                                target="_blank"
+                            >
+                                TraderJoe
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -71,10 +73,10 @@ const MainInfo = (props) => {
             </div>
             <div className="chart-wrapper">
                 <div>
-                    <RtChart/>
+                    <RtChart />
                 </div>
                 <p className="chart-description">
-                    Create a Nest with 10 $FIRE tokens to earn $FIRE rewards.<br/>
+                    Create a Nest with 10 $FIRE tokens to earn $FIRE rewards.<br />
                     You can use the rewards to create more Phoenix Nests and
                     grow your holding to a maximum of 100 Nests per wallet.
                 </p>
